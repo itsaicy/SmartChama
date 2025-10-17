ee7b03ae8544cbd8a719f2001dfe36e5e8d0a946
itsaicy <codewithmwema22@gmail.com>
2025-10-17 10:51:50 +0300

    registered all the urls for views

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user.views import home,signup_view,login_view,logout_view
+
+urlpatterns = [
+    path('', home),
+    path('login/',login_view, name='login'),
+    path('logout/',logout_view, name='logout'),
+    path('signup/',signup_view, name='signup'),
+]
