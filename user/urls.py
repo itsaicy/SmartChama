d7a79a276bdce2468fa3ca4c86047d6c68ae8f1d
itsaicy <codewithmwema22@gmail.com>
2025-10-20 09:41:31 +0300

    put a name to home path

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from user.views import home,signup_view,login_view,logout_view
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('login/',login_view, name='login'),
     path('logout/',logout_view, name='logout'),
     path('signup/',signup_view, name='signup'),
